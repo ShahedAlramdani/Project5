74b32189a74d529c8f086a01f518e375e20f1693
chuchu <chuchu@rands-air.lan>
2025-01-29 20:58:00 -0500

    implemented add method

--- src/Demo1/DemoTest5.java
+++ src/Demo1/DemoTest5.java
@@ -2,7 +2,15 @@ package Demo1;
 
 public class DemoTest5 {
 	public static void main (String args []) {
-		System.out.println("JAVA");
+		System.out.println("Shahed");
+		
+		
+		
+	}
+	
+	public void add(int a, int b) {
+		System.out.println(a+b);
+		
 		
 	}
 
